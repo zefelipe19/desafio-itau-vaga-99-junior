6838a6858b2a00bf15afb4d3c9c8e4823b1840fd
Felipe <felipe.paula1903@gmail.com>
2025-03-21 09:07:06 -0300

    criação da estrutura inicial do projeto

---
+++ src/main/java/dev/zefelipedev/desafioItauVaga99Junior/DesafioItauVaga99JuniorApplication.java
@@ -0,0 +1,13 @@
+package dev.zefelipedev.desafioItauVaga99Junior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauVaga99JuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioItauVaga99JuniorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/zefelipedev/desafioItauVaga99Junior/DesafioItauVaga99JuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zefelipedev.desafioItauVaga99Junior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauVaga99JuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
