9f222a6a33180422ff76092460b007d1d5de76b3
Felipe <felipe.paula1903@gmail.com>
2025-03-28 14:11:46 -0300

    Atualização dos atributos da TransacaoModel para os tipos de dados adequados

--- src/main/java/dev/zefelipedev/desafioItauVaga99Junior/TransacaoModel.java
+++ src/main/java/dev/zefelipedev/desafioItauVaga99Junior/TransacaoModel.java
@@ -1,25 +1,25 @@
 package dev.zefelipedev.desafioItauVaga99Junior;
 
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.util.Dictionary;
+import java.time.LocalDate;
+import java.time.OffsetTime;
+import java.time.ZonedDateTime;
 
 public class TransacaoModel {
     private Float valor;
-    private DateTimeFormat dataHora;
+    private ZonedDateTime dataHora;
 
     public TransacaoModel(){}
 
-    public TransacaoModel(Float valor, DateTimeFormat dataHora) {
+    public TransacaoModel(Float valor, String dataHora) {
         this.valor = valor;
-        this.dataHora = dataHora;
+        this.dataHora = ZonedDateTime.parse("2020-08-07T12:34:56.789-03:00");
     }
 
     public Float getValor() {
         return this.valor;
     }
 
-    public DateTimeFormat getDataHora() {
+    public ZonedDateTime getDataHora() {
         return this.dataHora;
     }
 }
